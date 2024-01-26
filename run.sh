255c4041a702e0cb394b13dbc4235ac89fd85cfb
HENG-A <151455114+HENG-A@users.noreply.github.com>
2024-01-26 17:15:19 +0800

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,12 +1,21 @@
+#!/bin/bash
+
+m_value="$1"
+
+
 echo "Enter M Value: "
-read m_value
+read -r m_value
+
 echo "Compiling simple_filter.c ..."
-c++ simple_filter.c -o simple_filter
+g++ simple_filter.c -o simple_filter
 echo "Done."
+
 echo "Running simple_filter.c ..."
-./simple_filter ${m_value} hL.txt hR.txt YL.txt YR.txt blue_giant_fragment.wav out.wav
+./simple_filter "${m_value}" hL.txt hR.txt YL.txt YR.txt blue_giant_fragment.wav out.wav
 echo "Done."
+
 echo "Running show_data.py ..."
-python show_data.py
+python3 show_data.py
 echo "Done. run.sh is done."
-read -p "Press any key to exit ..."
+
+read -p "Press Enter to exit ..."
